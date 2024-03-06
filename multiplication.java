27c1a5c5b4c4a79149ac0055fd4be8dc3d711859
Nibhafathima <151762426+Nibhafathima@users.noreply.github.com>
2024-03-06 15:37:23 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,13 @@
+class Math{
+public static void main(String args[])
+{
+int []arr;
+int i;
+for(i=1;i<=10;i++){
+arr=new int[11];
+arr[i]=5*i;
+System.out.println("5"+""+"*"+i+""+"="+arr[i]);
+}
+}
+}
+
